25b7d2b265784991b816976a951c9ece4c22a789
Daniel Arpide <edipra.tech@gmail.com>
2024-06-25 12:28:22 -0400

    updates

---
+++ links.sh
@@ -0,0 +1,9 @@
+ln -s ~/dotfiles/hypr ~/.config/hypr
+ln -s ~/dotfiles/i3 ~/.config/i3
+ln -s ~/dotfiles/polybar ~/.config/polybar
+ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/picom ~/.config/picom
+ln -s ~/dotfiles/neofetch ~/.config/neofetch
+ln -s ~/dotfiles/terminator ~/.config/terminator
+sudo ln -s ~/dotfiles/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
