bcb8185b651a023751853bb7f4c9adb1ae422a78
Daniel Arpide <edipra.tech@gmail.com>
2024-06-28 11:45:13 -0400

    nixos files

---
+++ configuration.nix
@@ -0,0 +1,216 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # Enable the hyprland Desktop Environment
+    programs.hyprland = {
+        enable = true;
+        xwayland = {
+            enable = true;
+        };
+    };
+
+  # Zsh Shell
+    users.defaultUserShell=pkgs.zsh;
+    programs.zsh.enable = true;
+    #programs.zsh = {
+    #  enable = true;
+    #  shellAliases = {
+    #    vim = "nvim";
+    #	ll = "ls -la";
+    #  };
+    #    autosuggestions.enable = true;
+    #   ohMyZsh = {
+    #	    theme = "refined";
+#	};
+#    };
+
+  # Configure keymap in X11
+    services.xserver = {
+        xkb.layout = "us";
+        xkb.variant = "";
+        enable = true;
+    }; 
+    services.displayManager.sddm = {
+	enable = true;
+    };
+    
+    # Docker
+    virtualisation.docker.enable = true;
+    virtualisation.docker.rootless = {
+        enable = true;
+        setSocketVariable = true;
+    };
+
+  # Virtualization
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.edipratech = {
+        isNormalUser = true;
+        description = "Daniel Arpide";
+        extraGroups = [ "networkmanager" "wheel" "video" "kvm" "libvirtd" "docker" ]; # Enable ‘sudo’ for the user.
+	shell = pkgs.zsh;
+            packages = with pkgs; [
+            ];
+    };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+	environment.systemPackages = with pkgs; [
+            brightnessctl
+            cargo
+            cmake
+	    distrobox
+            docker
+	    firefox
+            fzf
+            gcc
+	    gh
+            gimp
+            git
+            glibc
+            go
+            gnome.gnome-font-viewer
+            gparted
+            grim
+            gtk3
+	    home-manager
+            hyprland
+            libreoffice
+            libsecret
+            libvirt
+            lsd
+            lxappearance
+            meson
+            fastfetch 
+            ninja
+            networkmanagerapplet
+	    neovim
+	    oh-my-zsh
+            parsec-bin
+            pipewire
+            polkit_gnome
+            qemu_kvm
+	    rofi
+            rustup
+            sddm
+            slurp
+	    steam
+	    swaylock-effects
+	    terminator
+            tldr
+            unzip
+            virt-manager
+            vscode
+            waybar
+            wget
+            wireplumber
+	    wlsunset
+            wofi
+	    xfce.thunar
+            xwayland
+	    zsh
+	];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
